f2102e608908a629cdd35ac2da0c3bb703dc0fc6
Denis Chapligin <chollya@satgate.net>
2013-10-10 12:03:18 +0300

    Added copyright clauses to the source files.

--- src/js/form.js
+++ src/js/form.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2013 Denis V Chapligin.
+ * Distributed under the GPLv3.
+ * (See accompanying file LICENSE or copy at
+ * http://www.gnu.org/licenses/gpl-3.0.txt)
+ *
+ */
 define(["jsrender", "math", "simpson"],function (renderer, math, simpson) {
     return {
         validateFunction: function(selector) {
--- src/js/simpson.js
+++ src/js/simpson.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2013 Denis V Chapligin.
+ * Distributed under the GPLv3.
+ * (See accompanying file LICENSE or copy at
+ * http://www.gnu.org/licenses/gpl-3.0.txt)
+ *
+ */
 define(["math"], function(math) {
     /**
      * Implements Simpson's rule numerical integration algorithm,
